58e7558fb62d8ceb84764f13409f4a57b7939a0a
HIMANSHU DHIMAN <himanshudhimanhr@gmail.com>
2020-05-24 18:12:27 +0530

    login ui done

--- lib/login.dart
+++ lib/login.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 
 class LoginPage extends StatefulWidget {
   @override
@@ -14,75 +13,126 @@ class _LoginPageState extends State<LoginPage> {
     Size screenSize = MediaQuery.of(context).size;
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Welcome to the app!!!'),
-      ),
-      body: Container(
+        body: SafeArea(
+      child: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/back.png'), fit: BoxFit.cover),
+            color: Colors.red,
+          ),
           child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          SizedBox(height: 10),
-          Text(
-            'Login',
-            style: TextStyle(
-                color: Colors.blueGrey,
-                fontSize: 20,
-                fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 20),
-          ),
-          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Padding(
-                padding: EdgeInsets.only(left: 20),
-              ),
-              Flexible(
-                child: TextField(
-                  decoration: InputDecoration(hintText: 'Enter your email'),
+                padding: EdgeInsets.only(top: 40),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 20),
+                    ),
+                    Flexible(
+                      child: TextField(
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintStyle: TextStyle(color: Colors.white),
+                          hintText: 'Enter your email',
+                          icon: Icon(Icons.person, color: Colors.white),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 20),
+                    )
+                  ],
                 ),
               ),
               Padding(
-                padding: EdgeInsets.only(right: 20),
-              )
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 20),
-          ),
-          Row(
-            children: <Widget>[
-              Padding(
-                padding: EdgeInsets.only(left: 20),
-              ),
-              Flexible(
-                child: TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(hintText: 'Enter your password'),
+                padding: EdgeInsets.only(top: 20),
+                child: Row(
+                  children: <Widget>[
+                    Padding(
+                      padding: EdgeInsets.only(left: 20),
+                    ),
+                    Flexible(
+                      child: TextField(
+                        obscureText: true,
+                        style: TextStyle(color: Colors.white),
+                        decoration: InputDecoration(
+                          hintText: 'Enter your password',
+                          hintStyle: TextStyle(color: Colors.white),
+                          icon: Icon(Icons.lock, color: Colors.white),
+                          suffixIcon:
+                              Icon(Icons.visibility, color: Colors.white),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 20),
+                    )
+                  ],
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(right: 20),
-              )
-            ],
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 30),
-          ),
-          ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: FlatButton(
-                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
-                color: Colors.black,
-                onPressed: () {},
+              Container(
+                padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+                width: screenSize.width,
                 child: Text(
-                  'Login',
+                  'Forgot your password?',
                   style: TextStyle(color: Colors.white),
-                )),
-          )
-        ],
-      )),
-    );
+                  textAlign: TextAlign.right,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 10),
+              ),
+              Container(
+                  width: screenSize.width * 0.5,
+                  height: screenSize.width * 0.14,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(30),
+                    child: FlatButton(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        color: Color(0xFF5DCCC2),
+                        onPressed: () {},
+                        child: Text(
+                          'Login',
+                          style: TextStyle(color: Colors.white),
+                        )),
+                  )),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 30),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text('Don\'t have an account? ',
+                        style: TextStyle(color: Colors.white, fontSize: 16)),
+                    Text('Sign up',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold))
+                  ],
+                ),
+              ),
+            ],
+          )),
+    ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_cutter/login.dart';
 
 void main() {
@@ -9,7 +10,10 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: Color(0xFF5DCCC2)));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Cutter',
       theme: ThemeData(
         primarySwatch: Colors.blue,
